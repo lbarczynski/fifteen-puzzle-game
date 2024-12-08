ca6f245b3b2abb510e1a036495f1fae2cffa5adf
Łukasz Barczyński <barczynski.lukasz@gmail.com>
2024-12-08 11:56:56 +0100

    GitHub Actions - Build iOS app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
@@ -9,20 +6,4 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.kotlinCocoapods).apply(false)
     alias(libs.plugins.compose.compiler).apply(false)
-}
-
-allprojects {
-    setupTestLogging()
-}
-
-fun Project.setupTestLogging() {
-    println(this)
-    tasks.withType<Test> {
-        println(this)
-        testLogging {
-            events(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED)
-            exceptionFormat = TestExceptionFormat.FULL
-            showStandardStreams = true
-        }
-    }
 }
