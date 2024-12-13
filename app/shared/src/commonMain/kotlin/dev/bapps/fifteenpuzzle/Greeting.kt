917cd690c54e85e3bed3b05b935caf33c7dfac28
Łukasz Barczyński <barczynski.lukasz@gmail.com>
2024-12-13 22:10:42 +0100

    Game board API definied

--- app/shared/src/commonMain/kotlin/dev/bapps/fifteenpuzzle/Greeting.kt
+++ app/shared/src/commonMain/kotlin/dev/bapps/fifteenpuzzle/Greeting.kt
@@ -1,12 +1,9 @@
 package dev.bapps.fifteenpuzzle
 
-import dev.bapps.fifteenpuzzle.game.api.GameApi
-
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        val gameApi = GameApi()
-        return "Hello World, Platform: ${platform.name}! + ${gameApi.helloWorld()}"
+        return "Hello World, Platform: ${platform.name}! "
     }
 }
---
+++ game/api/src/commonMain/kotlin/dev/bapps/fifteenpuzzle/game/api/Board.kt
@@ -0,0 +1,23 @@
+package dev.bapps.fifteenpuzzle.game.api
+
+interface Board {
+    val rows: Int
+    val columns: Int
+
+    operator fun get(row: Int, column: Int): Int
+
+    fun canMove(direction: Direction): Boolean
+    fun move(direction: Direction)
+    fun isSolved(): Boolean
+    fun copy(): Board
+}
+
+fun Board.canMoveUp(): Boolean = canMove(Direction.Up)
+fun Board.canMoveDown(): Boolean = canMove(Direction.Down)
+fun Board.canMoveLeft(): Boolean = canMove(Direction.Left)
+fun Board.canMoveRight(): Boolean = canMove(Direction.Right)
+
+fun Board.moveUp() = move(Direction.Up)
+fun Board.moveDown() = move(Direction.Down)
+fun Board.moveLeft() = move(Direction.Left)
+fun Board.moveRight() = move(Direction.Right)
