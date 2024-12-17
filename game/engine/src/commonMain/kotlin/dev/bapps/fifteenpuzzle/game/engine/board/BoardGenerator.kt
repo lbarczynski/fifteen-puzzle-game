163ee6566afebf60d948692a2bb2123ba90de507
Łukasz Barczyński <barczynski.lukasz@gmail.com>
2024-12-17 23:35:47 +0100

    Random board generator

---
+++ game/engine/src/commonMain/kotlin/dev/bapps/fifteenpuzzle/game/engine/board/BoardGenerator.kt
@@ -0,0 +1,30 @@
+package dev.bapps.fifteenpuzzle.game.engine.board
+
+import dev.bapps.fifteenpuzzle.game.api.Board
+import kotlin.random.Random
+
+internal interface BoardGenerator {
+    fun random(rows: Int, columns: Int): Board
+}
+
+internal class ArrayBoardGenerator(
+    private val random: Random = Random.Default
+) : BoardGenerator {
+
+    override fun random(rows: Int, columns: Int): Board {
+        require(rows > 0) { "Rows must be greater than zero!" }
+        require(columns > 0) { "Columns must be greater than zero!" }
+
+        val values = (0 until rows * columns)
+            .toList()
+            .shuffled(random)
+
+        val board = Array(rows) { row ->
+            IntArray(columns) { column ->
+                values[row * columns + column]
+            }
+        }
+
+        return ArrayBoard(board)
+    }
+}
